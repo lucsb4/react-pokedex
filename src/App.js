82912c2c8310f11397a7d9de2b9a51efc7032d8c
Lucas <lucasbarbosa@outlook.com>
2021-01-14 15:09:54 -0300

    test commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useState, useEffect } from 'react';
+
 function App() {
+
+  const [pokedex, setPokedex] = useState([]);
+  const [wildPokemon, setWildPokemon] = useState({});
+
+  // useEffect is like componentDidMount
+  useEffect(() => {
+    
+  }, [pokedex]) // Will call every time the state "pokedex" updates
+
   return (
     <div className="App">
-      App test
+      App test, test!!
     </div>
   );
 }
